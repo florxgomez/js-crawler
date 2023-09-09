e6b8f64aa622eddde324e0ab8a71e27ed7c7dbb3
Florencia Gomez <florxgomez@gmail.com>
2023-09-08 23:39:36 -0300

    remove repeated call to sortPages

--- report.js
+++ report.js
@@ -7,7 +7,6 @@ function sortPages(pages) {
 
 function printReport(pages) {
   console.log('starting printing pages...');
-  sortPages(pages);
   for (const page of sortPages(pages)) {
     console.log(`Found ${pages[page]} internal links to ${page}`);
   }
